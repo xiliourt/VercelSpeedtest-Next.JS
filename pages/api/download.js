27b04eacdc8be0916bea5e6a16cae841b394a6d4
xiliourt <dyl.sec.ure@gmail.com>
2025-05-31 21:20:25 +1000

    Update download.js

--- pages/api/download.js
+++ pages/api/download.js
@@ -1,11 +1,3 @@
-// pages/api/download.js
-export const runtime = 'edge'; 
-
-// Configure this API route to run on the Edge Runtime
-export const config = {
-  runtime: 'edge',
-};
-
 // Function to generate a chunk of random data as Uint8Array
 function generateRandomChunk(size) {
   const buffer = new Uint8Array(size);
