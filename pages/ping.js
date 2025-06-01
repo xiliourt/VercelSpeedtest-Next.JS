0cf34789f858c2f53dce69bb2e62ac8818cedba5
xiliourt <dyl.sec.ure@gmail.com>
2025-06-01 13:53:17 +1000

    Update ping.js

--- pages/ping.js
+++ pages/ping.js
@@ -1,12 +1,7 @@
 import Head from 'next/head';
 
-export default function Ping() {
-  return (<head>
-      'Cache-Control': 'no-store, no-cache, must-revalidate, proxy-revalidate',
-      'Pragma': 'no-cache',
-      'Expires': '0',
-      'Surrogate-Control': 'no-store',
-      'Content-Type': 'text/plain',
-    </head>
-    OK);
+export default function ping() {
+  return OK
 }
+ 
+export default ping
