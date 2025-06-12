cb78180906d3eb78b373faf08fdf7950976b42dc
xiliourt <dyl.sec.ure@gmail.com>
2025-06-12 14:32:04 +1000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -10,12 +10,12 @@ async headers() {
                 },
                 {
                     key: "Access-Control-Allow-Methods",
-                    value: "GET,DELETE,PATCH,POST,PUT",
+                    value: "GET,POST",
                 },
                 {
                     key: "Access-Control-Allow-Headers",
                     value:
-                        "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+                        "Content-Type",
                 },
             ],
         },
