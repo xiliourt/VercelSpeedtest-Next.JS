4d78fcf499d7d087a5b8060ea2c51526c580fb86
xiliourt <dyl.sec.ure@gmail.com>
2025-06-12 14:31:36 +1000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,14 +6,7 @@ async headers() {
                 { key: "Access-Control-Allow-Credentials", value: "true" },
                 {
                     key: "Access-Control-Allow-Origin",
-                    value: (req) => {
-                        const allowedOrigins = ["https://js.vercel.dyl.ovh", "https://js.render.dyl.ovh", "https://js.aws.dyl.ovh", "https://js.syd.dyl.ovh", "http://js.syd.dyl.ovh" ];;
-                        const origin = req.headers.origin;
-                        if (allowedOrigins.includes(origin)) {
-                            return origin;
-                        }
-                        return "https://default.com"; // Or deny the request
-                    },
+                    value: "*",
                 },
                 {
                     key: "Access-Control-Allow-Methods",
