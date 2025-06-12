d2ce7a75c22d03ddf1ce0cad3d04df14bfa4d08d
xiliourt <dyl.sec.ure@gmail.com>
2025-06-12 14:23:42 +1000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,29 +1,27 @@
+
 module.exports = {
+
   async headers() {
     return [
-      {
-        source: '/:api*',
-        headers: [
-          {
-            key: 'access-control-allow-credentials',
-            value: 'true',
-          },
-          {
-            key: 'access-control-allow-headers',
-            value: 'Content-Type',
-          },
-          {
-            key: "Access-Control-Allow-Origin",
-            value: (req) => {
-              const allowedOrigins = ["https://js.vercel.dyl.ovh", "https://js.render.dyl.ovh", "https://js.aws.dyl.ovh", "https://js.syd.dyl.ovh", "http://js.syd.dyl.ovh" ];
-              const origin = req.headers.origin;
-              if (allowedOrigins.includes(origin)) {
-                return origin;
-            }
-              return "https://js.vercel.dyl.ovh";
-          }, 
-        ],
-    },
+        {
+            source: "/api/:path*",
+            headers: [
+                { key: "Access-Control-Allow-Credentials", value: "true" },
+                {
+                    key: "Access-Control-Allow-Origin",
+                    value: (req) => {
+                        const allowedOrigins = ["https://js.vercel.dyl.ovh", "https://js.render.dyl.ovh", "https://js.aws.dyl.ovh", "https://js.syd.dyl.ovh", "http://js.syd.dyl.ovh" ];
+                        const origin = req.headers.origin;
+                        if (allowedOrigins.includes(origin)) {
+                            return origin;
+                        }
+                        return "https://default.com"; // Or deny the request
+                    },
+                },
+                { key: "Access-Control-Allow-Headers", value: "Content-Type",
+                },
+            ],
+        },
     ];
-  },
-};
+}
+
