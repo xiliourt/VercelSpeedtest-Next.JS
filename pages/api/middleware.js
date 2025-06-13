636d2d11356ca624214af2b0d4a181583476f50b
xiliourt <dyl.sec.ure@gmail.com>
2025-06-14 07:49:48 +1000

    Update middleware.js

--- pages/api/middleware.js
+++ pages/api/middleware.js
@@ -1,10 +1,10 @@
 import { NextResponse } from 'next/server';
 import { get } from '@vercel/edge-config';
 
-export const config = { matcher: '/api/ping2' };
+export const config = { matcher: '/welcome' };
 
 export async function middleware() {
-  const greeting = await get('ok');
+  const greeting = await get('greeting');
   // NextResponse.json requires at least Next v13.1 or
   // enabling experimental.allowMiddlewareResponseBody in next.config.js
   return NextResponse.json(greeting);
