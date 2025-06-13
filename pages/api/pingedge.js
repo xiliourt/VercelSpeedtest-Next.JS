f182d39be80fa794f327f6e34f067d7d66c294fd
xiliourt <dyl.sec.ure@gmail.com>
2025-06-14 07:44:09 +1000

    Create pingedge.js

---
+++ pages/api/pingedge.js
@@ -0,0 +1,11 @@
+import { NextResponse } from 'next/server';
+import { get } from '@vercel/edge-config';
+
+export const config = { matcher: '/api/ping' };
+
+export async function middleware() {
+  const greeting = await get('ok');
+  // NextResponse.json requires at least Next v13.1 or
+  // enabling experimental.allowMiddlewareResponseBody in next.config.js
+  return NextResponse.json(greeting);
+}
