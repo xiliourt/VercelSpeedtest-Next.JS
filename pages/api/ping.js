b6590c4802942962caae1cd9d5529e11772cf4e9
xiliourt <dyl.sec.ure@gmail.com>
2025-06-14 07:42:12 +1000

    Update ping.js

--- pages/api/ping.js
+++ pages/api/ping.js
@@ -3,14 +3,11 @@ import { get } from '@vercel/edge-config';
 
 export const runtime = 'edge'; 
 
-// Configure this API route to run on the Edge Runtime
-export const config = {
-  runtime: 'edge',
-  matcher: '/api/ping' };
-  export async function middleware() {
+export const config = { runtime: 'edge', matcher: '/api/ping' };
+
+export async function middleware() {
   const greeting = await get('ok');
   // NextResponse.json requires at least Next v13.1 or
   // enabling experimental.allowMiddlewareResponseBody in next.config.js
   return NextResponse.json(greeting);
-  }
 }
