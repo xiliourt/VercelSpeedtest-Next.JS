034e5a0f61c63f1da635292f540f559ac2805aa1
xiliourt <dyl.sec.ure@gmail.com>
2025-06-14 07:40:33 +1000

    Update ping.js

--- pages/api/ping.js
+++ pages/api/ping.js
@@ -12,8 +12,8 @@ export const config = {
   // NextResponse.json requires at least Next v13.1 or
   // enabling experimental.allowMiddlewareResponseBody in next.config.js
   return NextResponse.json(greeting);
-  },
-};
+  }
+}
 
 export default function handler(req) {
   // On the Edge, the 'req' object is a standard Request object.
