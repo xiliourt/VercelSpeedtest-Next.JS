2e563405ee94029069b152560799f22aab84c64e
xiliourt <dyl.sec.ure@gmail.com>
2025-06-13 22:05:33 +1000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -34,7 +34,8 @@ const SERVERS = [
     { name: 'Netlify', pingUrl: 'https://js.netlify.dyl.ovh/api/ping', downloadUrl: 'https://js.netlify.dyl.ovh/api/download', uploadUrl: 'https://js.netlify.dyl.ovh/api/upload' },
     { name: 'Cloudflare', pingUrl: 'https://js.cf.dyl.ovh/api/ping', downloadUrl: 'https://js.cf.dyl.ovh/api/download', uploadUrl: 'https://js.cf.dyl.ovh/api/upload' },
     { name: 'Sydney, AU (Onidel)', pingUrl: 'https://js.s.dyl.ovh/api/ping', downloadUrl: 'https://js.s.dyl.ovh/api/download', uploadUrl: 'https://js.s.dyl.ovh/api/upload' },
-     { name: 'Stockholm (Hosthatch)', pingUrl: 'https://js.sto.dyl.ovh/api/ping', downloadUrl: 'https://js.sto.dyl.ovh/api/download', uploadUrl: 'https://js.sto.dyl.ovh/api/upload' }
+    { name: 'Stockholm (Hosthatch)', pingUrl: 'https://js.sto.dyl.ovh/api/ping', downloadUrl: 'https://js.sto.dyl.ovh/api/download', uploadUrl: 'https://js.sto.dyl.ovh/api/upload' },
+    { name: 'Stockholm (via CF)', pingUrl: 'https://js.stocf.dyl.ovh/api/ping', downloadUrl: 'https://js.stocf.dyl.ovh/api/download', uploadUrl: 'https://js.stocf.dyl.ovh/api/upload' }
 ];
 
 // --- TEST CONFIGURATION ---
