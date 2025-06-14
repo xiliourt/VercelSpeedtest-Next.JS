df667283be84c51b5ad1a25e154a3229fe747664
xiliourt <dyl.sec.ure@gmail.com>
2025-06-15 03:23:08 +1000

    Update download.js

--- pages/api/download.js
+++ pages/api/download.js
@@ -1,7 +1,5 @@
 // pages/api/ping.js
 export const runtime = 'edge'; 
-
-// Configure this API route to run on the Edge Runtime
 export const config = { runtime: 'edge', };
 
 // Function to generate a chunk of random data as Uint8Array
