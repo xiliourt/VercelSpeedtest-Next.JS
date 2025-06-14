e67daf517ea5337be218c93034c0d6c59f56560d
xiliourt <dyl.sec.ure@gmail.com>
2025-06-14 21:09:00 +1000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react';
-
 // --- ICONS (as SVG components for better control) ---
 const PlayIcon = () => (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
