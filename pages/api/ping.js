e3fed294a1a6a7f65023879562310da6d7df9ee5
xiliourt <dyl.sec.ure@gmail.com>
2025-06-14 12:46:51 +1000

    Update ping.js

--- pages/api/ping.js
+++ pages/api/ping.js
@@ -1,17 +1,9 @@
+import '@vercel/edge'
 export const runtime = 'edge'; 
 
-// Configure this API route to run on the Edge Runtime
-export const config = {
-  runtime: 'edge',
-};
+export const config = { runtime: 'edge', };
 
 export default function handler(req) {
-  // On the Edge, the 'req' object is a standard Request object.
-  // 'res' is not passed; you return a Response object.
-
-  // This endpoint simply needs to exist and respond quickly.
-  // The client measures the round-trip time.
-
   // Add headers to prevent caching by browser or intermediaries
   const headers = {
     'Cache-Control': 'no-store, no-cache, must-revalidate, proxy-revalidate',
