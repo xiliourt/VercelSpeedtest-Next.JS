aad831b5915956be15c1eaac72b17a8e77c21043
xiliourt <dyl.sec.ure@gmail.com>
2025-06-17 04:48:44 +1000

    Update ping2.js

--- pages/api/ping2.js
+++ pages/api/ping2.js
@@ -1,9 +1,11 @@
+import '@vercel/edge'
 import { NextResponse } from 'next/server';
 import { get } from '@vercel/edge-config';
 
+export const runtime = 'edge'; 
 export const config = { matcher: '/ping2' };
 
-export async function middleware() {
+default export async function middleware() {
   const greeting = await get('ping');
   // NextResponse.json requires at least Next v13.1 or
   // enabling experimental.allowMiddlewareResponseBody in next.config.js
