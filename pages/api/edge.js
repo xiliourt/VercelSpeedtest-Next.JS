4975917017d38bf614f1a8cc8a292a333f96d7cb
xiliourt <dyl.sec.ure@gmail.com>
2025-06-17 03:30:55 +1000

    Update edge.js

--- pages/api/edge.js
+++ pages/api/edge.js
@@ -1,11 +1,3 @@
-import { NextResponse } from 'next/server';
-import { get } from '@vercel/edge-config';
-
-export const config = { matcher: '/welcome' };
-
-export async function middleware() {
-  const greeting = await get('greeting');
-  // NextResponse.json requires at least Next v13.1 or
-  // enabling experimental.allowMiddlewareResponseBody in next.config.js
-  return NextResponse.json(greeting);
+export function GET(request: Request) {
+  return new Response('Hello from Vercel!');
 }
