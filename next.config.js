aa16e36a642456e4ecaf53de6052616fcc683e19
xiliourt <dyl.sec.ure@gmail.com>
2025-06-27 06:03:34 +1000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ const nextConfig = {
   async headers() {
     return [
       {
-        output: 'standalone',
         source: "/:api*",
         headers: [
           {
@@ -17,7 +16,7 @@ const nextConfig = {
           {
             key: "Access-Control-Allow-Headers",
             value: "X-Requested-With, Content-Type, Authorization",
-          },
+          }, {output: 'standalone'}
         ],
       },
     ];
