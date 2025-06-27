490b695bd73454026fd4966703d66240458f8c11
xiliourt <dyl.sec.ure@gmail.com>
2025-06-27 19:29:46 +1000

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -9,6 +9,7 @@ function MyApp({ Component, pageProps }) {
           href="[https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap](https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap)"
           rel="stylesheet"
         />
+        <link rel="icon" href="/favicon.png" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <Component {...pageProps} />
