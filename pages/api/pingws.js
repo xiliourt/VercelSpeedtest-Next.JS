3b5bb6d58b593681f03d049f45f5dffc29c68e75
xiliourt <dyl.sec.ure@gmail.com>
2025-06-27 11:08:32 +1000

    Update pingws.js

--- pages/api/pingws.js
+++ pages/api/pingws.js
@@ -3,15 +3,24 @@ export const runtime = 'edge';
 import { Server } from 'socket.io';
 
 export default function handler(req, res) {
-		if (!res.socket) { return ("pong") }
-		else if (!res.socket.server.io) {
+	const headers = {
+		    'Cache-Control': 's-maxage=3600',
+		    'Content-Type': 'text/plain',
+		    'Access-Control-Allow-Origin': '*',
+		    'Access-Control-Allow-Methods': 'GET',
+			'Access-Control-Allow-Headers': 'X-Requested-With, Content-Type, Authorization',
+		};
+
+	// If it's not a websocket connection (it's just waking up the server)
+	if (!res.socket) { return new Response('OK', {status: 200, headers: headers});) }
+	else if (!res.socket.server.io) {
 		const io = new Server(res.socket.server);
 		res.socket.server.io = io;
-			io.on('connection', (socket) => {
-	        socket.on('ping', (message) => {
+		io.on('connection', (socket) => {
+	    	socket.on('ping', (message) => {
 				socket.emit('pong', message);
 			});
-			socket.on('disconnect', () => {res.end()});
+		socket.on('disconnect', () => {res.end()});
 		});
 	}
 }
