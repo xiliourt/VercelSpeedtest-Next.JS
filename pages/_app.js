43a097017b6b9bcb4b5cdfc6b4381f2deadb9390
xiliourt <dyl.sec.ure@gmail.com>
2025-06-28 07:40:01 +1000

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -4,11 +4,6 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        {/* Link to Google Fonts for 'Inter'. Place this here for global font availability. */}
-        <link
-          href="[https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap](https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap)"
-          rel="stylesheet"
-        />
         <link rel="icon" href="/icon.png" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
