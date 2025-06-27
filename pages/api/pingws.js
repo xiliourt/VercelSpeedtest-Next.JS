0921b364aef8aa5d0fc99f1dc427939d6c6afb7d
xiliourt <dyl.sec.ure@gmail.com>
2025-06-27 11:01:34 +1000

    Update pingws.js

--- pages/api/pingws.js
+++ pages/api/pingws.js
@@ -3,15 +3,16 @@ export const runtime = 'edge';
 import { Server } from 'socket.io';
 
 export default function handler(req, res) {
-	if (!res.socket.server.io) {
-	const io = new Server(res.socket.server);
-	res.socket.server.io = io;
-		io.on('connection', (socket) => {
-        socket.on('ping', (message) => {
-			socket.emit('pong', message);
+		if (!res.socket.server.io) {
+		const io = new Server(res.socket.server);
+		res.socket.server.io = io;
+			io.on('connection', (socket) => {
+	        socket.on('ping', (message) => {
+				socket.emit('pong', message);
+			});
+			socket.on('disconnect', () => {res.end()});
 		});
-		socket.on('disconnect', () => {res.end()});
-	});
+	}
 }
   
   
