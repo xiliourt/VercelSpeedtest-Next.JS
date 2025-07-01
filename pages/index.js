501f1a5e17d82f974d1f0cac4d71d550ec6c79a1
xiliourt <dyl.sec.ure@gmail.com>
2025-07-01 21:17:59 +1000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -57,8 +57,7 @@ const SERVERS = [
     { name: 'Vercel (Edge CDN)', pingUrl: 'https://speedtestjs.vercel.app/api/ping', downloadUrl: 'https://speedtestjs.vercel.app/api/download', uploadUrl: 'https://speedtestjs.vercel.app/api/upload', maxUpload: '4194304'}, //4MB
     { name: 'Render', pingUrl: 'https://renderjsspeedtest.onrender.com/api/ping', downloadUrl: 'https://renderjsspeedtest.onrender.com/api/download', uploadUrl: 'https://renderjsspeedtest.onrender.com/api/upload', maxUpload: '27262976'}, // 26MB
     { name: 'Netlify (CDN)', pingUrl: 'https://speedtestnextjs.netlify.app/api/ping', downloadUrl: 'https://speedtestnextjs.netlify.app/api/download', uploadUrl: 'https://speedtestnextjs.netlify.app/api/upload', maxUpload: '4194304'}, // 4MB
-    { name: 'Cloudflare (Global CDN)', pingUrl: 'https://speedtestnextjs.pages.dev/api/ping', downloadUrl: 'https://speedtestnextjs.pages.dev/api/download', uploadUrl: 'https://speedtestnextjs.pages.dev/api/upload', maxUpload: '27262976' }, // 26MB
-    
+    { name: 'Cloudflare (Global CDN)', pingUrl: 'https://speedtestnextjs.pages.dev/api/ping', downloadUrl: 'https://speedtestnextjs.pages.dev/api/download', uploadUrl: 'https://speedtestnextjs.pages.dev/api/upload', maxUpload: '27262976' } // 26MB   
 ];
 
 // --- TEST CONFIGURATION ---
