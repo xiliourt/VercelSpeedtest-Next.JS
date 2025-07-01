ec58f9adf1571c9546c3431036a95aaa63e1cbc4
xiliourt <dyl.sec.ure@gmail.com>
2025-07-02 00:08:18 +1000

    Update download.js

--- pages/api/download.js
+++ pages/api/download.js
@@ -2,9 +2,11 @@ export const runtime = 'edge';
 export const config = { runtime: 'edge', };
 
 function generateRandomChunk(size) {
- const buffer = new Uint8Array(size);
- crypto.getRandomValues(buffer);
- return buffer;
+  const buffer = new Uint8Array(size);
+  for (let i = 0; i < size; i++) {
+    buffer[i] = i % 256;
+  }
+  return buffer;
 }
 
 
