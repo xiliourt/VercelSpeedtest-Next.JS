f257a25b20f5f59f956fde29323275909b83ff55
xiliourt <dyl.sec.ure@gmail.com>
2025-07-11 06:32:34 +1000

    Update servers.js

--- pages/api/servers.js
+++ pages/api/servers.js
@@ -18,18 +18,44 @@ export default function handler(req, res) {
     }
   }
 
-  /* If SERVERS_JSON fails, try using Vercel URL, else local host idk */
+  /*** If SERVERS_JSON fails...
+  ** Try using Vercel URL 
+  ** Try using Render URL
+  ** Try using Netlify URL
+  ** Try using Cloudflare Pages URL
+  ** Give up and use localhost:3000 */
   const vercelUrl = process.env.NEXT_PUBLIC_VERCEL_URL;
   loadEnvConfig(vercelUrl)
   if (servers.length === 0 && vercelUrl) {
     console.warn("Using Vercel URL as a fallback server configuration.");
-    servers = [ { name: 'Vercel (This Deployment)', pingUrl: `https://${vercelUrl}/api/ping`, downloadUrl: `https://${vercelUrl}/api/download`, uploadUrl: `https://${vercelUrl}/api/upload`, maxUpload: '4194304' } ];
-  }
-  
-  if (servers.length === 0) {
-    console.warn("Using localhost as the final fallback server configuration.");
-    servers = [ { name: 'Localhost', pingUrl: 'http://127.0.0.1:3000/api/ping', downloadUrl: 'http://127.0.0.1:3000/api/download', uploadUrl: 'http://127.0.0.1:3000/api/upload', maxUpload: '27262976' } ];
+    servers = [ { name: 'Vercel (This Deployment)', serverUrl: `https://${vercelUrl}`, maxUpload: '4194304' } ];
   }
 
+  const renderUrl = process.env.RENDER_EXTERNAL_URL
+  loadEnvConfig(renderUrl)
+  if (servers.length === 0 && renderUrl) {
+    console.warn("Using Vercel URL as a fallback server configuration.");
+    servers = [ { name: 'Render (this deployment)', serverUrl: `https://${renderUrl}`, maxUpload: '4194304' } ];
+  }
+
+  const netlifyUrl = process.env.DEPLOY_PRIME_URL
+  loadEnvConfig(netlifyUrl)
+  if (servers.length === 0 && netlifyUrl) {
+    console.warn("Using Vercel URL as a fallback server configuration.");
+    servers = [ { name: 'Netlifgy (this deployment)', serverUrl: `https://${netlifyUrl}`, maxUpload: '4194304' } ];
+  }
+
+  const CFPagesURL = process.env.CF_PAGES_URL
+  loadEnvConfig(CFPagesURL)
+  if (servers.length === 0 && CFPagesURL) {
+    console.warn("Using Vercel URL as a fallback server configuration.");
+    servers = [ { name: 'Netlifgy (this deployment)', serverUrl: `https://${CFPagesURL}`, maxUpload: '4194304' } ];
+  }
+  
+  /* Fuck it, using local host as the URL */
+  if (servers.length === 0) {
+    console.error("Using localhost as the final fallback server configuration. You didn't set SERVERS_JSON as an environment variable");
+    servers = [ { name: 'Localhost', serverUrl: 'http://127.0.0.1:3000', maxUpload: '27262976'}] ;
+  }
   res.status(200).json(servers);
 }
