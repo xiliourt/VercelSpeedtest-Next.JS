d996391e184f4683c25e70020a825a4e649aacad
xiliourt <dyl.sec.ure@gmail.com>
2025-07-11 04:48:25 +1000

    Update servers.js

--- components/servers.js
+++ components/servers.js
@@ -1,10 +1,13 @@
+import { loadEnvConfig } from '@next/env'
 export async function getStaticProps() {
   let SERVERS = [];
   
   const serversJson = process.env.SERVERS_JSON;
+  loadEnvConfig(serversJson)
   const serverUrlVercel = process.env.NEXT_PUBLIC_VERCEL_URL
+  loadEnvConfig(serverUrlVercel)
   if (serversJson) {
-    try { SERVERS = [serversJson]; } catch (error) { 
+    try { SERVERS = JSON.parse(serversJson); } catch (error) { 
         console.error(error);
         const SERVERS = [{ name: 'Localhost', pingUrl: 'http://127.0.0.1:3000/api/ping', downloadUrl: 'http://127.0.0.1:3000/api/download', uploadUrl: 'http://127.0.0.1:3000/api/upload', maxUpload: '27262976' } ];
     }
