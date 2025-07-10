be99371b34962b713d659865f6546e59a68b627d
xiliourt <dyl.sec.ure@gmail.com>
2025-07-11 04:41:01 +1000

    Update servers.js

--- components/servers.js
+++ components/servers.js
@@ -7,9 +7,9 @@ export async function getStaticProps() {
     try {
       SERVERS = JSON.parse(serversJson); 
     } catch (error) { 
-        const SERVERS = [{ name: 'Azure (Free AppService)', pingUrl: 'http://127.0.0.1:3000/api/ping', downloadUrl: 'http://127.0.0.1:3000/api/download', uploadUrl: 'http://127.0.0.1:3000/api/upload', maxUpload: '27262976' } ];
+        const SERVERS = [{ name: 'Localhost', pingUrl: 'http://127.0.0.1:3000/api/ping', downloadUrl: 'http://127.0.0.1:3000/api/download', uploadUrl: 'http://127.0.0.1:3000/api/upload', maxUpload: '27262976' } ];
     }
   } else {
-        const SERVERS = [{ name: 'Azure (Free AppService)', pingUrl: 'http://127.0.0.1:3000/api/ping', downloadUrl: 'http://127.0.0.1:3000/api/download', uploadUrl: 'http://127.0.0.1:3000/api/upload', maxUpload: '27262976' } ];
+        const SERVERS = [{ name: 'Localhost', pingUrl: 'http://127.0.0.1:3000/api/ping', downloadUrl: 'http://127.0.0.1:3000/api/download', uploadUrl: 'http://127.0.0.1:3000/api/upload', maxUpload: '27262976' } ];
   }
 }
