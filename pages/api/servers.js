54998e2fe0ee772ffced7f3b90c4e62fd6a339b1
xiliourt <dyl.sec.ure@gmail.com>
2025-07-11 05:33:09 +1000

    Update servers.js

--- pages/api/servers.js
+++ pages/api/servers.js
@@ -16,5 +16,5 @@ export default function handler(req, res) {
         console.error("No servers and not running on Vercel, using local host")
         const SERVERS = [{ name: 'Localhost', pingUrl: 'https://127.0.0.1/api:3000/ping', downloadUrl: 'http:/127.0.0.1:3000/api/download', uploadUrl: 'https://127.0.0.1:3000/api/upload', maxUpload: '27262976' } ];
   }
-  res.status(200).json({ {JSON.stringify(SERVERS)} });
+  res.status(200).json(JSON.stringify(SERVERS));
 }
