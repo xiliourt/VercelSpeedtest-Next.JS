125359d8fe936485ccb61bcd20f4cc136302f591
xiliourt <dyl.sec.ure@gmail.com>
2025-07-11 04:40:45 +1000

    Update servers.js

--- components/servers.js
+++ components/servers.js
@@ -1,15 +1,15 @@
-let SERVERS = [];
-
-const serversJson = process.env.SERVERS_JSON;
-
-if (serversJson) {
-  try {
-    SERVERS = JSON.parse(serversJson); 
-  } catch (error) { 
-      const SERVERS = [{ name: 'Azure (Free AppService)', pingUrl: 'http://127.0.0.1:3000/api/ping', downloadUrl: 'http://127.0.0.1:3000/api/download', uploadUrl: 'http://127.0.0.1:3000/api/upload', maxUpload: '27262976' } ];
+export async function getStaticProps() {
+  let SERVERS = [];
+  
+  const serversJson = process.env.SERVERS_JSON;
+  
+  if (serversJson) {
+    try {
+      SERVERS = JSON.parse(serversJson); 
+    } catch (error) { 
+        const SERVERS = [{ name: 'Azure (Free AppService)', pingUrl: 'http://127.0.0.1:3000/api/ping', downloadUrl: 'http://127.0.0.1:3000/api/download', uploadUrl: 'http://127.0.0.1:3000/api/upload', maxUpload: '27262976' } ];
+    }
+  } else {
+        const SERVERS = [{ name: 'Azure (Free AppService)', pingUrl: 'http://127.0.0.1:3000/api/ping', downloadUrl: 'http://127.0.0.1:3000/api/download', uploadUrl: 'http://127.0.0.1:3000/api/upload', maxUpload: '27262976' } ];
   }
-} else {
-      const SERVERS = [{ name: 'Azure (Free AppService)', pingUrl: 'http://127.0.0.1:3000/api/ping', downloadUrl: 'http://127.0.0.1:3000/api/download', uploadUrl: 'http://127.0.0.1:3000/api/upload', maxUpload: '27262976' } ];
 }
-
-module.exports = SERVERS;
