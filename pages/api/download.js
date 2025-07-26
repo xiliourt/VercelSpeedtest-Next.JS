45ac41652493547c4682046f7b1241a3e24de09e
xiliourt <dyl.sec.ure@gmail.com>
2025-07-26 15:05:21 +1000

    Update download.js

--- pages/api/download.js
+++ pages/api/download.js
@@ -18,7 +18,7 @@ export default async function handler(req) {
   const chunkSize = 64 * 1024; // 64KB chunks
 
   const headers = {
-    'Content-Type': 'text/plain',
+    'Content-Type': 'application/octet-stream',
     'Content-Disposition': 'attachment; filename="download.dat"',
     'Content-Length': requestedSize.toString(),
   };
